f99d3350772e2224f8cff495d0fe3136c17fb919
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-10-20 10:45:32 -0300

    header component

--- dashboard/src/Components/Header.tsx
+++ dashboard/src/Components/Header.tsx
@@ -1,11 +1,18 @@
 import DateRange from "./DateRange";
+import Meses from "./Meses";
+import React from 'react';
 
 export default function Header(){
 
+    const [title, setTitle] = React.useState('Resumo');
 
     return (
-        <div>
-            <DateRange/>
-        </div>
+        <header className="mb">
+            <div className="daterange mb">
+                <DateRange/>
+                <h1 className="box bg-3">{title}</h1>
+            </div>
+            <Meses/>
+        </header>
     )
 }
