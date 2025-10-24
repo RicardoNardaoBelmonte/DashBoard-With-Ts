f1f26d09a083b992bbc333fc1b2b5d6c6d00299d
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-10-24 14:50:19 -0300

    add navLink instead tag <a>

--- dashboard/src/Components/SideNav.tsx
+++ dashboard/src/Components/SideNav.tsx
@@ -5,6 +5,7 @@ import configuracoes from '../assets/icons/configuracoes.svg';
 import contato from '../assets/icons/contato.svg';
 import sair from '../assets/icons/sair.svg';
 import LogoSvg from '../assets/LogoSvg';
+import { NavLink } from 'react-router-dom';
 
 export default function SideNav(){
     return (
@@ -13,11 +14,11 @@ export default function SideNav(){
             <ul>
                 <li>
                     <span><img src={resumo} alt="" /></span>
-                    <a href="">Resumo</a>
+                    <NavLink to='/'>Resumo</NavLink>
                 </li>
                 <li>
                     <span><img src={vendas} alt="" /></span>
-                    <a href="">Vendas</a>
+                    <NavLink to='/vendas'>Vendas</NavLink>
                 </li>
                 <li>
                     <span><img src={webhooks} alt="" /></span>
