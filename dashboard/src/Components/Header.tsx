895c670da213632085271284dd855f8fa616b1b4
Ricardo Nardao Belmonte <nardaoricardo@gmail.com>
2025-10-24 14:51:44 -0300

    add useEffect to set the page title and document title

--- dashboard/src/Components/Header.tsx
+++ dashboard/src/Components/Header.tsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom";
 import DateRange from "./DateRange";
 import Meses from "./Meses";
 import React from 'react';
@@ -5,6 +6,17 @@ import React from 'react';
 export default function Header(){
 
     const [title, setTitle] = React.useState('Resumo');
+    const location = useLocation();
+
+    React.useEffect(() => {
+        if(location.pathname === "/"){  
+            setTitle("Resumo")
+            document.title = 'Deashboard | Resumo'
+        }else if(location.pathname === '/vendas'){
+            document.title = 'Deashboard | Vendas'
+            setTitle("Vendas")
+        }
+    },[location])
 
     return (
         <header className="mb">
